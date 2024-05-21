eea391e62eae515b2a16c99f1201807d349bc7dc
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-05-21 11:11:51 -0700

    Create 4 basic functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 // java script
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(number, numberToSubtract) {
+    return number- numberToSubtract;
+}
+
+function multiply(num1,num2) {
+    return num1*num2;
+}
+
+function divide(numerator, divisor) {
+    if (divisor === 0){
+        return "ERROR";
+    } else return numerator/divisor;
+}
